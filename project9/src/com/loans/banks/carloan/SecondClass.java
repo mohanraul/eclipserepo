5169be5e8979e11e2ac6c749316dfff1644e8a2f
mohanraul <mohancraul@gmail.com>
2019-11-24 19:22:00 +0530

    Added First Line

--- project9/src/com/loans/banks/carloan/SecondClass.java
+++ project9/src/com/loans/banks/carloan/SecondClass.java
@@ -3,7 +3,7 @@ package com.loans.banks.carloan;
 public class SecondClass {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		System.out.println("Added First Line");
 
 	}
 
